aaaa83a462d50db699415f919f86141ca378fdc1
anonymous <anuwong@dollarsign.co>
2022-06-09 18:14:05 +0700

    read port from env

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "OK")
 	})
-	r.Run("")
+	r.Run(":" + port)
 }
